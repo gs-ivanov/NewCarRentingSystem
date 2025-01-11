c76f2cdd439211ff20f418d9ad776013acf84a9b
George <gs.ivanov@abv.bg>
2025-01-11 08:45:17 +0200

    Two tables (Category & Cars) migrated.

--- CarRentingSystem/Infrastructure/ApplicationBuilderExtensions.cs
+++ CarRentingSystem/Infrastructure/ApplicationBuilderExtensions.cs
@@ -1,9 +1,11 @@
 ﻿namespace CarRentingSystem.Infrastructure
 {
     using CarRentingSystem.Data;
+    using CarRentingSystem.Data.Models;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.DependencyInjection;
+    using System.Linq;
 
     public static class ApplicationBuilderExtensions
     {
@@ -16,7 +18,30 @@
 
             data.Database.Migrate();
 
+            SeedCategories(data);
+
             return app;
         } 
+
+        private static void SeedCategories(CarRentingDbContext data)
+        {
+            if (data.Categories.Any())
+            {
+                return;
+            }
+
+            data.Categories.AddRange(new[]
+            {
+                new Category{Name="Mini"},
+                new Category{Name="Economy"},
+                new Category{Name="Midsize"},
+                new Category{Name="Large"},
+                new Category{Name="SUV"},
+                new Category{Name="Vans"},
+                new Category{Name="Luxory"},
+            });
+
+            data.SaveChanges();
+        }
     }
 }
