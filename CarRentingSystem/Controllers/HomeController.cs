47f5bc86d1150910e0997dce6531feec12bd4cc8
George <gs.ivanov@abv.bg>
2025-01-16 18:10:15 +0200

    Implemented Slide view in Index page.

--- CarRentingSystem/Controllers/HomeController.cs
+++ CarRentingSystem/Controllers/HomeController.cs
@@ -1,44 +1,46 @@
 ﻿namespace CarRentingSystem.Controllers
 {
-    using System.Diagnostics;
-    using System.Linq;
     using CarRentingSystem.Data;
     using CarRentingSystem.Models;
-    using CarRentingSystem.Models.Cars;
+    using CarRentingSystem.Models.Home;
     using Microsoft.AspNetCore.Mvc;
+    using System.Diagnostics;
+    using System.Linq;
 
     public class HomeController : Controller
     {
         private readonly CarRentingDbContext data;
 
         public HomeController(CarRentingDbContext data)
-        {
-            this.data = data;
-        }
+            => this.data = data;
 
-
-        public IActionResult Index() 
+        public IActionResult Index()
         {
+            var totalCars = this.data.Cars.Count();
+
             var cars = this.data
                .Cars
-               //.OrderByDescending(c => c.Id)
-               .Select(c => new CarListingViewModel
+               .OrderByDescending(c => c.Id)
+               .Select(c => new CarIndexViewModel
                {
                    Id = c.Id,
                    Brand = c.Brand,
                    Model = c.Model,
                    Year = c.Year,
                    ImageUrl = c.ImageUrl,
-                   Category = c.Category.Name
                })
                .Take(3)
                .ToList();
 
-            return View(cars);
+            return View(new IndexViewModel
+            {
+                Cars=cars,
+                TotalCars=totalCars
+            });
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        
-        public IActionResult Error()=>View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
+        public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
